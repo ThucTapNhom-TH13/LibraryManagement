6be340070b0d90a334552a53fda305309485810a
phuonglinhth238 <phuonglinhth238@gmail.com>
2017-06-05 09:27:29 +0700

    test

---
+++ LibraryManagement/huongdan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Quanlythuvien
+{
+    public partial class huongdan : Form
+    {
+        public huongdan()
+        {
+            InitializeComponent();
+        }
+
+        private void huongdan_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("");
+        }
+    }
+}
